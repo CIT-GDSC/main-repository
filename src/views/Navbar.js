e38a316548485dc47b8b099a77bd0cd5cc336be1
Domains18 <gibsonsgibson88@gmail.com>
2023-03-16 23:47:58 +0300

    dependencies and nav

---
+++ src/views/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <span>Coast Institute of Technology</span>
+            </div>
+            <div className="items">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
